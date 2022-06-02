58f03943e04306338b83c5b758c767dd704050de
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-06-02 11:26:18 -0300

    one more time

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,11 @@
 import Image from 'next/image';
 import VerifyContainer from '../Components/VerifyContainer';
-
-
+import logoEinstein from '../public/Einstein Floripa horizontal.png';
 
 export default function Home() {
   return (
     <main className='flex flex-col items-center h-screen w-screen p-8 bg-blue-300'>
-      <Image
-        src='/../public/Einstein Floripa horizontal.png'
-        alt='logo-einstein'
-        width={270}
-        height={130}
-      />
+      <Image src={logoEinstein} alt='logo-einstein' width={270} height={130} />
       <VerifyContainer />
     </main>
   );
