e390774a9a659df27052584630333dad8d0ee2df
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-09-13 16:46:05 -0300

    fix: arrumando problema com ssl

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,7 @@
-import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta
-          httpEquiv='Content-Security-Policy'
-          content='upgrade-insecure-requests'
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
