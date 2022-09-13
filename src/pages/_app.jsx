19f469a063cc614a92bc5b758ebc7dec3d2e7414
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-09-13 16:26:45 -0300

    fix: arrumando bug de https

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          httpEquiv='Content-Security-Policy'
+          content='upgrade-insecure-requests'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
