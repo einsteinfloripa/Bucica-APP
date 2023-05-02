96425a3530b9594b87e9d4ca77e8878c6691522b
Gabriel Viana Boaes <gvb@certi.org.br>
2023-05-02 10:56:02 -0300

    fix: type problem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-import '../styles/globals.css'
+import { AppProps } from "next/app";
+import "../styles/globals.css";
 
-import 'normalize.css'
+import "normalize.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
