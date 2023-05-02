f42f28a8553d0ef30af002cad5809973eda8dafe
Gabriel Viana <viana_gab2001@hotmail.com>
2023-05-02 10:57:44 -0300

    feat: add login business logic (#7)
    * feat: add login business logic
    
    * fix: type problem
    
    ---------
    
    Co-authored-by: Gabriel Viana Boaes <gvb@certi.org.br>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,11 @@ import { useRouter } from "next/router";
 
 import logoEinstein from "../../public/Einstein Floripa horizontal.png";
 
+import { useAuth } from "@hooks";
+
 export default function Home() {
   const router = useRouter();
+  const { login } = useAuth();
 
   return (
     <main className='h-screen w-screen bg-einstein bg-center bg-cover'>
@@ -22,11 +25,30 @@ export default function Home() {
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            //@ts-ignore
-            localStorage.setItem("username", e?.target[0].value);
-            //@ts-ignore
-            localStorage.setItem("password", e?.target[1].value);
-            router.push("/bucicaqr");
+            // @ts-ignore
+            const username = e?.target[0].value;
+            // @ts-ignore
+            const password = e?.target[1].value;
+
+            const onSuccess = () => {
+              localStorage.setItem("username", username);
+              localStorage.setItem("password", password);
+              router.push("/bucicaqr");
+            };
+
+            const onError = () => {
+              localStorage.clear();
+              alert("Usuário ou senha incorretos");
+            };
+
+            login(
+              {
+                username,
+                password,
+              },
+              onSuccess,
+              onError,
+            );
           }}
           className='relative flex flex-col items-center justify-around mt-10 bg-white w-80 h-80 rounded-2xl'
         >
@@ -36,7 +58,7 @@ export default function Home() {
           />
           <input
             className='flex justify-center items-center border-2 border-slate-200 w-60 h-16 rounded-xl font-montserrat font-bold text-center'
-            type="password"
+            type='password'
             placeholder='Senha'
           />
           <button className='flex justify-center items-center bg-softblue w-60 h-16 rounded-3xl font-montserrat font-bold text-white  cursor-pointer'>
--- src/services/EinsteinApiClient/index.ts
+++ src/services/EinsteinApiClient/index.ts
@@ -3,12 +3,14 @@ import axios, { AxiosInstance } from "axios";
 class EinsteinApiClient {
   protected readonly api: AxiosInstance;
   public readonly attendance: Attendance;
+  public readonly auth: Auth;
 
   constructor() {
     this.api = axios.create({
       baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
     });
     this.attendance = new Attendance(this.api);
+    this.auth = new Auth(this.api);
   }
 }
 
@@ -23,9 +25,32 @@ class Attendance {
         auth: {
           username: localStorage.getItem("username") || "",
           password: localStorage.getItem("password") || "",
-        }
+        },
       },
     );
+    return response;
+  }
+}
+
+export type LoginData = {
+  username: string;
+  password: string;
+};
+class Auth {
+  constructor(protected readonly api: AxiosInstance) {}
+
+  async login({ username, password }: LoginData) {
+    const response = await this.api.post(
+      `/login`,
+      {},
+      {
+        auth: {
+          username: username,
+          password: password,
+        },
+      },
+    );
+
     return response;
   }
 }
